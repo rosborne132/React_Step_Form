db63ed5efbc200cad7ce58af4cf2bb5d3190bde1
ozborne132 <ozborne132@gmail.com>
2018-11-07 20:14:14 -0800

    Updates

---
+++ precache-manifest.2dacf19fd425078ec50c24452ed2ebe0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1025a438fcccbc2145f7",
+    "url": "/rosborne132/React_Step_Form/static/js/runtime~main.1025a438.js"
+  },
+  {
+    "revision": "fdb90858e9ec2efe1df7",
+    "url": "/rosborne132/React_Step_Form/static/js/main.fdb90858.chunk.js"
+  },
+  {
+    "revision": "33995e7aaa06d4e2521b",
+    "url": "/rosborne132/React_Step_Form/static/js/1.33995e7a.chunk.js"
+  },
+  {
+    "revision": "fdb90858e9ec2efe1df7",
+    "url": "/rosborne132/React_Step_Form/static/css/main.5ae1f70f.chunk.css"
+  },
+  {
+    "revision": "164dfffe1d4af80e2d388dde33765131",
+    "url": "/rosborne132/React_Step_Form/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/rosborne132/React_Step_Form/precache-manifest.2dacf19fd425078ec50c24452ed2ebe0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/rosborne132/React_Step_Form/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
